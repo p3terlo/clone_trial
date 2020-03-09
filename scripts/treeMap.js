f6bb0cf79bafb8ba6142be02f4bec892a74528e3
Peter Lo <pelo@ucdavis.edu>
2020-03-09 14:02:00 -0700

    Added sector titles to tree map

--- scripts/treeMap.js
+++ scripts/treeMap.js
@@ -2,7 +2,7 @@ function TreeMap(svg,data){
 
     this.svg = svg;
     let boundingBox = svg.node().getBoundingClientRect();
-    let margin = {top: 20, bottom: 20, left: 20, right: 20}
+    let margin = {top: 10, bottom: 10, left: 10, right: 10}
     let svgHeight = boundingBox.height;
     let svgWidth = boundingBox.width;
     let width = svgWidth - margin.left - margin.right;
@@ -21,6 +21,9 @@ function TreeMap(svg,data){
     
     d3.treemap()
         .size([width,height])
+        .paddingTop(25)
+        .paddingRight(5)
+        .paddingInner(0)
         (root)
 
     let color = d3.scaleOrdinal()
@@ -31,7 +34,37 @@ function TreeMap(svg,data){
         .data(root.leaves())
         .enter()
         .append('rect')
+        .attr('x', function (d) { return d.x0; })
+        .attr('y', function (d) { return d.y0; })
         .attr('width', function (d) { return d.x1 - d.x0; })
         .attr('height', function (d) { return d.y1 - d.y0; })
+        .style("stroke", "black")
         .attr('fill', function(d) { return color(d.parent.data.name); } )
+
+    // myGroup.selectAll("text")
+    //     .data(root.leaves())
+    //     .enter()
+    //     .append("text")
+    //       .attr("x", function(d){ return d.x0+5})    // +10 to adjust position (more right)
+    //       .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
+    //       .text(function(d){ return d.data.name; })
+    //       .attr("font-size", "19px")
+    //       .attr("fill", "white")
+
+    myGroup.selectAll("titles")
+        .data(root.descendants().filter(function(d){return d.depth==1}))
+        .enter()
+        .append("text")
+        .attr("x", function(d){ return d.x0})
+        .attr("y", function(d){ return d.y0+21})
+        .text(function(d){ return d.data.name })
+        .attr("font-size", "15px")
+        .attr("fill", "black")
+
+    myGroup.append("text")
+        .attr("x", width/2 - 100)
+        .attr("y", 14)    // +20 to adjust position (lower)
+        .text("S&P 500 Companies by Sector")
+        .attr("font-size", "19px")
+        .attr("fill",  "grey" )
 }
